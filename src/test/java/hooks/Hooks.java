fe4399f92679e9d278c3f44a7c6cb772bbb5afb2
anshit01jan <41467755+anshit01jan@users.noreply.github.com>
2026-08-07 19:43:32 +0530

    Enhance teardown with screenshot on failure
    Capture screenshots on scenario failure and add to report.

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,12 +1,17 @@
 package hooks;
 
+import com.aventstack.extentreports.ExtentTest;
 import framework.drivers.DriverFactory;
 import framework.drivers.DriverManager;
 import framework.reports.ExtentManager;
+import framework.reports.ExtentTestManager;
 import framework.utils.Config;
+import framework.utils.ScreenshotUtil;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+
+import java.util.UUID;
 
 public class Hooks {
 
@@ -19,11 +24,22 @@ public class Hooks {
     @Before(order = 1)
     public void setup(Scenario scenario) {
         DriverFactory.initDriver(System.getProperty("browser"));
+        // Scenario node is created by ExtentCucumberPlugin (TestCaseStarted)
     }
 
     @After
     public void tearDown(Scenario scenario) {
-        DriverManager.quitDriver();
+        try {
+            if (scenario.isFailed()) {
+                String path = ScreenshotUtil.capture("FAILED_" + UUID.randomUUID() + ".png");
+                ExtentTest test = ExtentTestManager.getTest();
+                if (test != null && path != null) {
+                    test.addScreenCaptureFromPath(path);
+                }
+            }
+        } finally {
+            DriverManager.quitDriver();
+        }
     }
 
     @After(order = 1000)

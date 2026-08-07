b2c16567e00fcb7a5d0885866b575ed395e8c989
anshit01jan <41467755+anshit01jan@users.noreply.github.com>
2026-08-07 16:33:32 +0530

    feat: add DashboardPage POM

---
+++ src/main/java/framework/pages/DashboardPage.java
@@ -0,0 +1,15 @@
+package framework.pages;
+
+import framework.base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.SeleniumWebDriverWait;
+
+public class DashboardPage extends BasePage {
+    public DashboardPage(WebDriver driver, SeleniumWebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public String getPath() {
+        return java.net.URI.create(driver.getCurrentUrl()).getPath();
+    }
+}

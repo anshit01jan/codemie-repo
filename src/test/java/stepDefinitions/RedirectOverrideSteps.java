964c4cb4faac58f1b5d17228d505c0daeccd3a38
anshit01jan <41467755+anshit01jan@users.noreply.github.com>
2026-08-07 18:45:26 +0530

    fix: add RedirectOverrideSteps for not-redirected assertion (workaround broken LoginStepDefinitions)

---
+++ src/test/java/stepDefinitions/RedirectOverrideSteps.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+import framework.drivers.DriverManager;
+import io.cucumber.java.en.Then;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.WebDriver;
+
+import java.net.URI;
+
+public class RedirectOverrideSteps {
+    @Then("the user should not be redirected to the Dashboard page")
+    public void the_user_should_not_be_redirected() {
+        WebDriver driver = DriverManager.getDriver();
+        Assertions.assertThat(driver).is.notNull();
+        String path = URI.create(driver.getCurrentUrl()).getPath();
+        Assertions.assertThat(path).isNOtEqualTo("/dashboard");
+    }
+}

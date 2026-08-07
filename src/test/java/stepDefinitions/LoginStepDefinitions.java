0c09b494064d9d7319d2ad70e0bc8160583f1901
anshit01jan <41467755+anshit01jan@users.noreply.github.com>
2026-08-07 17:45:44 +0530

    micro-patch B1: shrink and standardize step definitions base

--- src/test/java/stepDefinitions/LoginStepDefinitions.java
+++ src/test/java/stepDefinitions/LoginStepDefinitions.java
@@ -1,93 +1,38 @@
 package stepDefinitions;
 
 import framework.drivers.DriverManager;
-import framework.pages.DashboardPage;
 import framework.pages.LoginPage;
 import framework.utils.Config;
-
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
 import org.assertj.core.api.Assertions;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URI;
 import java.time.Duration;
 
 public class LoginStepDefinitions {
     private WebDriver driver;
     private WebDriverWait wait;
     private LoginPage loginPage;
-    private DashboardPage dashboardPage;
-
-    private String username;
-    private String password;
 
     private void initPages() {
-        if (DriverManager.getDriver() == null) {
-            throw new IllegalStateException("WebDriver is null. Check Hooks.java for driver init.");
-        }
         driver = DriverManager.getDriver();
+        if (driver == null) {
+            throw new IllegalStateException("WebDriver is null. Check hooks/driver init.");
+        }
         wait = new WebDriverWait(driver, Duration.ofSeconds(Config.getTimeoutSeconds())));
         loginPage = new LoginPage(driver, wait);
-        dashboardPage = new DashboardPage(driver, wait);
-    }
-
-    private String currentPath() {
-        return URI.create(driver.getCurrentUrl()).getPath();
     }
 
     @Given("the user is on the Login page")
-    public void user_on_login_page() {
+    public void user_on_login() {
         initPages();
         loginPage.open();
     }
 
-    @And("the user navigates to the Login page")
-    public void user_navigates_to_login() {
-        user_on_login_page();
-    }
-
-    @And("the user has a valid username \"(^)+)\"")
-    public void set_username(String user) {
-        this.username = user;
-    }
-
-
-    @And("the user has an invalid username \"(~+)\"")
-    public void set_invalid_username(String user) {
-        this.username = user;
-    }
-
-    @And("the user has a valid password \"(~+)\"")
-    public void set_password(String pass) {
-        this.password = pass;
-    }
-
-
-    @And("the user has an invalid password \"(~+)\"")
-    public void set_invalid_password(String pass) {
-        this.password = pass;
-    }
-
-
-    @When("the user clicks on the Login button")
-    public void the_user_clicks_on_the_login_button() {
-        loginPage.login(username, password);
-    }
-
-    @Then("the user should be redirected to the Dashboard page \"(~+)\"")
-    public void redirected_to_dashboard(String path) {
-        Assertions.assertThat(dashboardPage.getPath()).isEqualTo(path);
-    }
-
-
-    @Then("the user should see an error message \"(~+)\"")
-    public void see_error(msg) {
+    @Then("the user should see an error message {string}")
+    public void user_sees_error(String msg) {
         Assertions.assertThat(loginPage.getAlertText()).contains(msg);
     }
 }

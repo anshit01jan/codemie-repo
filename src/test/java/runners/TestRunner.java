f016f4318902ef2f6783c60aa4e879fdc89a5b52
anshit01jan <41467755+anshit01jan@users.noreply.github.com>
2026-08-07 17:23:49 +0530

    test: implement Cucumber TestNG runner with parallel execution

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,3 +1,20 @@
 package runners;
 
-public class TestRunner {}
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"hooks", "stepDefinitions"},
+    plugins = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"},
+    monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[] scenarios() {
+        return super.scenarios();
+    }
+}

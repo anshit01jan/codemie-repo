ea6c6ac80735dfe52b11c55c7237470a31f86317
anshit01jan <41467755+anshit01jan@users.noreply.github.com>
2026-08-07 19:47:30 +0530

    Replace SeleniumWebDriverWait with WebDriverWait

--- src/main/java/framework/pages/DashboardPage.java
+++ src/main/java/framework/pages/DashboardPage.java
@@ -2,10 +2,10 @@ package framework.pages;
 
 import framework.base.BasePage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.SeleniumWebDriverWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DashboardPage extends BasePage {
-    public DashboardPage(WebDriver driver, SeleniumWebDriverWait wait) {
+    public DashboardPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
 

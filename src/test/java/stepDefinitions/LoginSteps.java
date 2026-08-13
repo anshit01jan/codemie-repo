62402dfd31ebfb68a7a79a016bc4e9c2933fee9f
anshit01jan <41467755+anshit01jan@users.noreply.github.com>
2026-08-13 12:20:51 +0530

    Refactor LoginSteps by cleaning up code
    Removed unused imports and variables, simplified page initialization.

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -5,16 +5,9 @@ import framework.pages.DashboardPage;
 import framework.pages.LoginPage;
 import framework.utils.Config;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.assertj.core.api.Assertions;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.net.URI;
 import java.time.Duration;
 
 public class LoginSteps {
@@ -22,23 +15,14 @@ public class LoginSteps {
     private WebDriver driver;
     private WebDriverWait wait;
     private LoginPage loginPage;
-    private DashboardPage dashboardPage;
-
-    private String username;
-    private String password;
-
     private void initPages() {
         if (DriverManager.getDriver() == null) {
             throw new IllegalStateException("WebDriver is null. Check hooks/driver initialization.");
         }
         driver = DriverManager.getDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(Config.getTimeoutSeconds())));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(Config.getTimeoutSeconds()));
         loginPage = new LoginPage(driver, wait);
-        dashboardPage = new DashboardPage(driver, wait);
-    }
-
-    private String currentPath() {
-        return URI.create(driver.getCurrentUrl()).getPath();
+        new DashboardPage(driver, wait);
     }
 
     // --- Login flow ---
@@ -53,5 +37,4 @@ public class LoginSteps {
     public void user_navigates_to_login() {
         user_on_login_page();
     }
-
-    @End
+}
